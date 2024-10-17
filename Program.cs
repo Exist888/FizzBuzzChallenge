339fed739bec79058e327cb96ce0ef354f9fc57d
Filip Herbst <filip_herbst@yahoo.com>
2024-10-17 15:06:03 -0500

    FizzBuzzChallenge Completed

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        FizzBuzz(9);
+        FizzBuzz(15);
+        FizzBuzz(7);
+        FizzBuzz(3);
+        FizzBuzz(10);
+        FizzBuzz(8);
+        FizzBuzz(30);
+    }
+
+    public static string FizzBuzz(int number)
+    {
+        if (number % 5 == 0 && number % 3 == 0)
+        {
+            Console.WriteLine("FizzBuzz");
+            return "FizzBuzz";
+        }
+        else if (number % 3 == 0 && number % 5 != 0)
+        {
+            Console.WriteLine("Fizz");
+            return "Fizz";
+        }
+        else if (number % 5 == 0 && number % 3 != 0)
+        {
+            Console.WriteLine("Buzz");
+            return "Buzz";
+        }
+        else
+        {
+            Console.WriteLine("Try again");
+            return "Try Again";
+        }
+    }
+}
